a3e9762aef80e80ec3cbdc491c24ffe7eca9ea97
kust <ekater.kust@gmail.com>
2023-02-09 22:00:07 +0500

    Little adjustment for EnemyBullet

--- ScrollShooter/EnemyBullet.cpp
+++ ScrollShooter/EnemyBullet.cpp
@@ -4,12 +4,12 @@
 EnemyBullet::EnemyBullet(std::shared_ptr<Game> game, const gm::Coord& pos, const gm::Vector& dir) :
 	Enemy(game, "EnemyBullet", pos) {
 	normalOffset = dir.normal() * 0.2;
-	health = -1; // negative means undead
+	health = -1; // negative means immortal
 	collisionLayers.reset();
 	collisionLayers[2] = 1;
 	auto center = hitbox->getCenter();
 	hitbox->setSize({ 4,4 });
-	hitbox->setCenter(center);
+	hitbox->setCenter(pos);
 }
 
 void EnemyBullet::dropBonus() const {}
